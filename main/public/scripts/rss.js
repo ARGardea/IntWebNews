a1286f1cc47f6bc4bb69d3f63b757926c8bc4a82
Alejandro <AlexRogerG@gmail.com>
2015-12-14 10:00:50 -0700

    Timer and Manual working

--- main/public/scripts/rss.js
+++ main/public/scripts/rss.js
@@ -3,16 +3,52 @@ var rssReader = (function () {
         targetDiv, rssData;
 
     return {
+        addBit: function (bitType, className, addTarget, bitContent) {
+            var newSpan = document.createElement(bitType);
+            newSpan.setAttribute('class', className);
+            newSpan.innerHTML = bitContent;
+            addTarget.appendChild(newSpan);
+        },
+        makeBreak: function (breakTarget) {
+            var br = document.createElement('br');
+            breakTarget.appendChild(br);
+        },
         loadRssData: function (data) {
             rssData = data;
             console.log('Data loaded!');
-            console.log(data);
+            if (data.responseData.feed && data.responseData.feed.entries) {
+
+                data.responseData.feed.entries.forEach(function (entry) {
+                    var newDiv = document.createElement('div');
+                    newDiv.setAttribute('class', 'rssBit');
+                    rssReader.addBit('span', 'titleSpan', newDiv, entry.title);
+                    rssReader.makeBreak(newDiv);
+                    rssReader.addBit('span', 'pubSpan', newDiv, 'Published ' + entry.publishedDate);
+                    rssReader.makeBreak(newDiv);
+                    rssReader.addBit('span', 'snipSpan', newDiv, entry.contentSnippet);
+
+                    rssReader.makeBreak(newDiv);
+                    var anchorBit = document.createElement('a');
+                    anchorBit.setAttribute('href', entry.link);
+                    anchorBit.innerHTML = 'Read More';
+                    newDiv.appendChild(anchorBit);
+
+                    targetDiv.appendChild(newDiv);
+                });
+            }
         },
         run: function () {
             console.log('RSS script now running!');
             targetDiv = document.getElementById('rss');
-            $.get(targetUrl, function(data){
-                rssReader.loadRssData(data);
+            //            $.get(targetUrl, function(data){
+            //                rssReader.loadRssData(data);
+            //            });
+            $.ajax({
+                url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(targetUrl),
+                dataType: 'jsonp',
+                success: function (data) {
+                    rssReader.loadRssData(data);
+                }
             });
         }
     }
---
+++ main/public/scripts/time.js
@@ -0,0 +1,70 @@
+var classTimer = (function () {
+    var dayTime;
+
+    return {
+        manualMode: false,
+        setup: function () {
+            this.checkTime();
+            setInterval(classTimer.update, 1000);
+            document.getElementById('timeTrigger').onclick = this.triggerManual;
+            document.getElementById('autoTrigger').onclick = this.triggerAuto;
+        },
+        checkTime: function () {
+            var currentHour = new Date().getHours();
+            console.log(currentHour);
+            if (currentHour >= 5 && currentHour < 17) {
+                console.log("It's Day!");
+                if (!dayTime) {
+                    dayTime = true;
+                    this.toggleTime(dayTime);
+                }
+            } else {
+                console.log("It's Night!");
+                if (dayTime) {
+                    dayTime = false;
+                    this.toggleTime(dayTime);
+                }
+            }
+        },
+        triggerManual: function () {
+            classTimer.manualMode = true;
+            classTimer.switchTime();
+        },
+        switchTime: function () {
+            dayTime = !dayTime;
+            this.toggleTime(dayTime);
+        },
+        triggerAuto: function () {
+            classTimer.manualMode = false;
+        },
+        toggleTime: function (isDay) {
+            var set = document.getElementsByClassName('timeMe');
+            var dayClass = 'day',
+                nightClass = 'night',
+                addedClass, removedClass;
+            if (isDay) {
+                addedClass = dayClass;
+                removedClass = nightClass;
+            } else {
+                addedClass = nightClass;
+                removedClass = dayClass;
+            }
+            for (var i = 0; i < set.length; i++) {
+                var item = set[i];
+                item.classList.add(addedClass);
+                item.classList.remove(removedClass);
+                //                item.classList.add('HI');
+                //                item.classList.remove('timeMe');
+            }
+            console.log(set);
+        },
+        update: function () {
+            if (!classTimer.manualMode) {
+                classTimer.checkTime();
+            }
+        },
+        run: function () {
+            classTimer.setup();
+        }
+    }
+}());
