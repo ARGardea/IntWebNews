29fa9f33575728976a4be470dfd83833043ea917
Alexander Devine <alexdevine387@gmail.com>
2015-12-14 21:28:08 -0700

    Added 5 day forecast

--- devine.js
+++ devine.js
@@ -1,6 +1,8 @@
 // MY API KEY: 943239fa0e94ea682b788f358aa0a448 
 
 var weatherData;
+var city = 'Salt Lake City'
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 var request = new XMLHttpRequest();
 
@@ -17,24 +19,45 @@ $.ajax({
 });
 
 function loadWeather() {
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=5780993&units=imperial&APPID=943239fa0e94ea682b788f358aa0a448');
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + ',us&units=imperial&APPID=943239fa0e94ea682b788f358aa0a448');
     request.onload = loadComplete;
     request.send();
+}
+
+function setCity(cityName) {
+    city = cityName;
+    loadWeather();
+    console.log(cityName);
+    console.log(city);
+
 }
 
 function loadComplete(evt) {
     weatherData = JSON.parse(request.responseText);
     console.log(weatherData);
-    var so = new Date(weatherData.list[0].dt * 1000);
-    document.getElementById("date").innerHTML = weatherData.list[0].weather[0].description;
-    document.getElementById("wDescription").innerHTML = so.getHours();
-    document.getElementById("humidity").innerHTML = weatherData.list[0].main.humidity + "%";
-    document.getElementById("temp").innerHTML = weatherData.list[0].main.temp + "F";
+    document.getElementById("curCity").innerHTML = weatherData.city.name;
+    document.getElementById('weather').innerHTML = "";
+    for (i = 0; i < 5; i++) {
+        var so = new Date(weatherData.list[i].dt * 1000);
+        var dayDiv = document.createElement('div');
+        
+        var wd = document.createTextNode(weatherData.list[i].weather[0].description);
+        var d = document.createTextNode(months[so.getMonth()] + " " + so.getDate());
+        var hum = document.createTextNode('Humidity: ' + weatherData.list[i].humidity + "%");
+        var tem = document.createTextNode('Temp: ' + weatherData.list[i].temp.max + "F°");
+        
+        dayDiv.appendChild(wd);
+        dayDiv.appendChild(d);
+        dayDiv.appendChild(hum);
+        dayDiv.appendChild(tem);
+        
+        document.getElementById('weather').appendChild(dayDiv);
+        
+    }
 }
 
 function clockTicker() {
     var now = new Date();
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
     date = months[now.getMonth()] + ", " + now.getDate() + " " + now.getFullYear();
 
